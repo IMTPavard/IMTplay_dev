eb0b1b7d54be5ad0bb7ab291129bb4879230b56d
ivan <ivanrigal@outlook.fr>
2021-02-21 13:47:59 +0100

    initial setup

---
+++ orchestrator/startup.sh
@@ -0,0 +1,12 @@
+find /etc/nginx/sites-enabled/* | while read -r line ; do rm $line; done
+#cat nginx_conf.conf > /etc/nginx/sites-available/myconf
+cat nginx_conf.conf > /etc/nginx/nginx.conf
+#ln -s /etc/nginx/sites-available/myconf /etc/nginx/sites-enabled
+if [ ! -d /var/www/app ]; then
+    mkdir /var/www/app
+fi
+chown webuser /var/www/app
+#gunicorn --workers 3 --bind unix:myproject.sock -m 007 wsgi:app --daemon
+#/etc/init.d/nginx start
+#chown webuser *
+#/etc/init.d/nginx start
